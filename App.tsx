ac9b3d08d8f3f964eb2f7fc17ee883d8f6a74e42
tatva-kashmeera <kashmeera.vora@tatvasoft.com>
2024-01-22 00:53:31 +0530

    instagram reels UI and functionality

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity, Alert, Dimensions, Image } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Image, Pressable } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 //@ts-ignore
 import Video, { OnProgressData } from 'react-native-video';
@@ -17,6 +17,8 @@ export default function App() {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [isLoaded, setIsLoaded] = useState<boolean>(false)
   const [isMuted, setIsMuted] = useState<boolean>(false)
+  const [speackerShow, setSpeackerShow] = useState<boolean>(false)
+  const [like, setLike] = useState<boolean>(false)
 
 
   useEffect(() => {
@@ -26,23 +28,20 @@ export default function App() {
       setPaused(false)
       console.log("videoRef.current", currentIndex, currentTime)
     }
-    if(isLoaded && currentIndex !== -1 && currentIndex > 0){
-      Alert.alert("call")
+    if (isLoaded && currentIndex !== -1 && currentIndex > 0) {
       setIsLoaded(false)
     }
-  }, [currentIndex,isLoaded])
+  }, [currentIndex, isLoaded])
 
   const onBuffer = (e: any) => {
-    // console.log("buffering....", e)
+    console.log("buffering....", e)
   }
 
   const videoError = (e: any) => {
-    // console.log("video error....", e)
+    console.log("video error....", e)
 
   }
-  const onPlayPausePress = () => {
-    setPaused(!paused);
-  };
+
 
   const onProgress = (data: OnProgressData) => {
 
@@ -57,7 +56,7 @@ export default function App() {
     return { minutes, seconds: remainingSeconds };
   }
 
-  const onLoad = (data: { duration: number }, index:number, currentIndex:number) => {
+  const onLoad = (data: { duration: number }, index: number, currentIndex: number) => {
     if (!isLoaded && index !== currentIndex) {
       console.log("get onload data...", data.duration)
       const { minutes, seconds } = secondsToMinutesAndSeconds(data.duration);
@@ -68,28 +67,20 @@ export default function App() {
 
   };
 
-  const calculateProgress = () => {
-    if (duration !== undefined && duration !== 0) {
-      return (currentTime / duration) * 100;
-    }
-    return 0;
-  };
-
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time % 60);
-    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  };
-
-
   const videoPlayer = (item: ArrayVideo, index: number) => {
     return (
-      <TouchableOpacity
-      onPress={()=>setIsMuted(!isMuted)}
-      style={{
-        height: height,
-        width: width
-      }}>
+      <Pressable
+        onPress={() => {
+          setIsMuted(!isMuted),
+            setSpeackerShow(true)
+          setTimeout(() => {
+            setSpeackerShow(false)
+          }, 1000)
+        }}
+        style={{
+          height: height,
+          width: width
+        }}>
         <Video source={{ uri: item.sources }}
           ref={videoRef}
           onBuffer={onBuffer}
@@ -98,46 +89,58 @@ export default function App() {
           paused={index !== currentIndex || paused}
           onProgress={onProgress}
           muted={isMuted}
-          onLoad={(data: {duration:number})=> onLoad(data,index, currentIndex)}
+          onLoad={(data: { duration: number }) => onLoad(data, index, currentIndex)}
           style={styles.backgroundVideo} />
-
-        <View style={{ position: "absolute", bottom: 30, left: 20, right: 20, }}
-        >
-          <View style={styles.progressContainer}>
-            <View
-              style={{ ...styles.progressBar, width: `${calculateProgress()}%` }}
-            />
-          </View>
-
-
+        <View style={{ position: "absolute", bottom: 50, right: 6 }}>
           <TouchableOpacity
-            onPress={onPlayPausePress}>
-            <Text>{paused ? 'Play' : 'Pause'}</Text>
-          </TouchableOpacity>
+            onPress={() => setLike(!like)}
+          >{
+              like ?
+                <Image style={[styles.icon3, { tintColor: "red" }]} source={require("./assets/heartfill.png")} />
+                :
+                <Image style={styles.icon3} source={require("./assets/heart.png")} />
 
-          <Text>
-            Current Time: {formatTime(currentTime)} / Total Time:{' '}
-            {duration !== undefined ? formatTime(duration) : 'Loading...'}
-          </Text>
+            }
+          </TouchableOpacity>
+          <Text style={styles.countLike}>449 k</Text>
+          <Image style={styles.icon2} source={require("./assets/message.png")} />
+          <Text style={styles.countLike}>2,168</Text>
+          <Image style={styles.icon2} source={require("./assets/paperplane.png")} />
+          <Text style={styles.countLike}>191 k</Text>
+          <Image style={styles.icon1} source={require("./assets/ellipsis.png")} />
+
         </View>
-        <View style={styles.imagView}>
-      {
-        isMuted ? 
-        <Image style={styles.speacker} source={require("./assets/speackerOff.png")} />
-        :
-        <Image style={styles.speacker} source={require("./assets/speackerOn.png")} />
-      }
-      </View>
-      </TouchableOpacity>
+
+        <View style={{ position: "absolute", bottom: 30 }}
+        >
+<View style={styles.nameRow}>
+<Image style={styles.profile} source={require("./assets/profile.jpeg")} />
+<Text style={styles.tittle}>{item.title}</Text>
+<TouchableOpacity style={styles.followBtn}>
+  <Text style={[styles.tittle, {marginLeft:0}]}>Follow</Text>
+</TouchableOpacity>
+</View>
+
+<Text numberOfLines={1}
+style={styles.discription}
+>{item.description}</Text>
+             
+        </View>
+        {speackerShow && <View style={styles.imagView}>
+          {
+            isMuted ?
+              <Image style={styles.speacker} source={require("./assets/speackerOff.png")} />
+              :
+              <Image style={styles.speacker} source={require("./assets/speackerOn.png")} />
+          }
+        </View>}
+      </Pressable>
     )
   }
 
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
       <SwiperFlatList
-        // autoplay
-        // autoplayDelay={20000}
-        // autoplayLoop={false}
         index={currentIndex}
         vertical
         data={videoArray}
@@ -147,7 +150,7 @@ export default function App() {
         }}
         renderItem={({ item, index }) => videoPlayer(item, index)}
       />
-     
+
     </View>
   )
 }
@@ -158,21 +161,73 @@ const styles = StyleSheet.create({
     width: width
   },
   progressContainer: {
-    height: 10,
-    backgroundColor: '#ccc',
+    height: 2,
+    backgroundColor: '#f9f9f9',
     marginTop: 10,
+    borderColor: "#f9f9f9",
+    width: width
   },
   progressBar: {
     height: '100%',
     backgroundColor: 'blue',
   },
-  imagView:{
-   justifyContent:"center",
-  //  position:"absolute"
+  imagView: {
+    justifyContent: "center",
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 0,
+    top: 0,
   },
-  speacker:{
-    
-    height:120,
-    width:20
+  speacker: {
+    height: 50,
+    width: 50,
+  },
+  icon1: {
+    height: 6,
+    width: 30,
+    marginTop:40
+  },
+  icon2: {
+    height: 26,
+    width: 26,
+  },
+  icon3: {
+    height: 26,
+    width: 26,
+  },
+  nameRow: {
+    flexDirection: 'row',
+    alignItems:'center',
+    marginLeft:16,
+  },
+  profile: {
+    width:40,
+    height:40,
+    borderRadius:20
+  },
+  tittle:{
+    color:"#fff",
+    fontSize:14,
+    marginLeft:10,
+    fontWeight:"700"
+  },
+  followBtn: {
+    borderRadius:4,
+    borderColor: "#f9f9f9",
+    borderWidth:0.5,
+    marginLeft:10,
+    paddingVertical: 4,
+    paddingHorizontal:8
+  },
+  discription:{
+    marginRight:60,
+    marginLeft:20,
+    marginTop:10,
+    color:"#fff"
+  },
+  countLike: {
+    color:"#fff",
+    marginTop:8,
+    marginBottom:20
   }
 });
